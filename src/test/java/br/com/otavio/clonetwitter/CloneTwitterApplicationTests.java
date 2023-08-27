4c8ad0036536d6a216f5f5801db7846294a6f7e7
Otavio Marques <marquesotavio251204@gmail.com>
2023-08-27 14:46:25 -0300

    init

---
+++ src/test/java/br/com/otavio/clonetwitter/CloneTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.otavio.clonetwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
